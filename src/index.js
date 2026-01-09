5c065979aab58a65c25b6e527e6fb26e2298f6ce
Fabio Tomas Naene <fabiotomasnaene@gmail.com>
2026-01-09 11:27:43 +0200

    functions for display pages was called

--- src/index.js
+++ src/index.js
@@ -1,1 +1,45 @@
 import '../src/styles.css';
+import { createHeaderElementsHomePage,
+    createrMainElementsHomePage,
+    createFooterElementsHomePage
+} from './homePage.js';
+import { createAboutPageElements } from './aboutPage.js';  
+import { createMenuPageElements } from './menuPage.js';  
+
+
+createHeaderElementsHomePage();
+createrMainElementsHomePage();
+createFooterElementsHomePage();
+
+const main = document.querySelector('main');
+const buttonHome = document.querySelector('#button-home');
+const buttonMenu = document.querySelector('#button-menu');
+const buttonAbout = document.querySelector('#button-about');
+const buttons = document.querySelectorAll('header nav button');
+
+function setActiveButton() {
+    buttons.forEach(button => {
+        button.classList.remove('active');
+    });
+};
+
+buttonHome.addEventListener('click', (event) => {
+    main.innerHTML = ''; 
+    setActiveButton();
+    buttonHome.classList.add('active');
+    createrMainElementsHomePage();
+});
+
+buttonMenu.addEventListener('click', (event) => {
+    main.innerHTML = ''; 
+    setActiveButton();
+    buttonMenu.classList.add('active');
+    createMenuPageElements();
+});
+
+buttonAbout.addEventListener('click', (event) => {
+    main.innerHTML = ''; 
+    setActiveButton();
+    buttonAbout.classList.add('active');
+    createAboutPageElements();
+});
